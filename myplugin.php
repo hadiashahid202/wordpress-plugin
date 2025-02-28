5f4c05e48646dc7fc9d26a46255557dea6566494
Hadia Shahid <hadiashahid202@gmail.com>
2025-02-28 14:51:57 +0500

    plugin

---
+++ myplugin.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: My Basic Plugin
+Plugin URI: http://yourwebsite.com
+Description: A simple custom WordPress plugin.
+Version: 1.0
+Author: Your Name
+Author URI: http://yourwebsite.com
+License: GPL2
+*/
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Function to display a custom message
+function myplugin_custom_message() {
+    return "<p style='padding:10px; background:#f4f4f4; border-left:5px solid #0073aa;'>Hello! This is a message from MyPlugin.</p>";
+}
+
+// Shortcode to use the function
+function myplugin_register_shortcode() {
+    add_shortcode('myplugin_message', 'myplugin_custom_message');
+}
+add_action('init', 'myplugin_register_shortcode');
+?>
